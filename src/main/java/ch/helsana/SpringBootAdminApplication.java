af4e65b6e013ad58449a6499468992c7e51c5240
Urs Wiss <urs.wiss@gmail.com>
2017-05-22 12:15:46 +0200

    Initial commit

---
+++ src/main/java/ch/helsana/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package ch.helsana;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+}
