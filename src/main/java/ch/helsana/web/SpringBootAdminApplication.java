87e972c066f0cabdbad55494aed386c2adf38792
Marcel Widmer <marzelwidmer@gmail.com>
2017-06-06 14:14:18 +0200

    Revert "add spring-security 1st version for api-gateway protection under /manage/admin/spring-boot-admin"
    This reverts commit eb3559fbe8daacc67894c1fd464d11d7448f0f3e.

--- src/main/java/ch/helsana/web/SpringBootAdminApplication.java
+++ src/main/java/ch/helsana/web/SpringBootAdminApplication.java
@@ -4,13 +4,11 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 
 //@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableGlobalAuthentication
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
