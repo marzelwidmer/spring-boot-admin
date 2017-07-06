a927dfe37237933be749db1a2974d2e426422d07
Urs Wiss <urs.wiss@gmail.com>
2017-07-06 17:33:57 +0200

    add-basic-auth-to-spring-boot-admin

--- src/main/java/ch/helsana/web/SpringBootAdminApplication.java
+++ src/main/java/ch/helsana/web/SpringBootAdminApplication.java
@@ -1,10 +1,15 @@
 package ch.helsana.web;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.discovery.ApplicationDiscoveryListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableAutoConfiguration
@@ -12,7 +17,17 @@ import org.springframework.security.config.annotation.authentication.configurati
 @EnableGlobalAuthentication
 public class SpringBootAdminApplication {
 
+	@Autowired
+	private ApplicationDiscoveryListener listener;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAdminApplication.class, args);
 	}
-}
+
+	@PostConstruct
+	public void init() {
+		// we have to fire this event in order to trigger the service registration
+		InstanceRegisteredEvent<?> event = new InstanceRegisteredEvent<>("prod", null);
+		// for some reason publising doesn't work, so we invoke directly
+		listener.onInstanceRegistered(event);
+	}}
