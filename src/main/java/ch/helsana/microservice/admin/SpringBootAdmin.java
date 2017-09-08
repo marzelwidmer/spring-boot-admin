1008cc3c7c00f3f01513786a364f77bbc5ccffd5
Marcel Widmer <marzelwidmer@gmail.com>
2017-09-08 14:28:57 +0200

    CleanUp Configuration

--- src/main/java/ch/helsana/microservice/admin/SpringBootAdmin.java
+++ src/main/java/ch/helsana/microservice/admin/SpringBootAdmin.java
@@ -1,35 +1,22 @@
 package ch.helsana.microservice.admin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
-import de.codecentric.boot.admin.discovery.ApplicationDiscoveryListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.client.discovery.event.InstanceRegisteredEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
 @EnableGlobalAuthentication
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class SpringBootAdmin {
 
-	@Autowired
-	private ApplicationDiscoveryListener listener;
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAdmin.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		// we have to fire this event in order to trigger the service registration
-		InstanceRegisteredEvent<?> event = new InstanceRegisteredEvent<>("prod", null);
-		// for some reason publising doesn't work, so we invoke directly
-		listener.onInstanceRegistered(event);
-	}}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdmin.class, args);
+    }
+}
