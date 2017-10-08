53a474431b5e04167d69814d64ec10549deaab2a
Marcel Widmer <marzelwidmer@gmail.com>
2017-10-08 08:59:30 +0200

    first commit

---
+++ src/main/java/ch/helsana/microservice/admin/SpringBootAdmin.java
@@ -0,0 +1,25 @@
+package ch.helsana.microservice.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableGlobalAuthentication
+//@EnableEurekaClient
+@EnableDiscoveryClient
+public class SpringBootAdmin {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdmin.class, args);
+    }
+}
+
+
+
+
